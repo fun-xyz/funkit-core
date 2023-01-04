3d88eafc73ea9a7cb595e2fee0f9480dc776d642
aaronchewbani <aaronchewbani@gmail.com>
2023-01-04 13:02:59 -0800

    firebase stuff

--- sdk/interaction/src/ExposedSDK.ts
+++ sdk/interaction/src/ExposedSDK.ts
@@ -1,121 +1,10 @@
-
-import { ethers, BigNumber, BigNumberish } from 'ethers'
-import { Provider } from '@ethersproject/providers'
-import {
-  EntryPoint, EntryPoint__factory,
-  UserOperationStruct
-} from '@account-abstraction/contracts'
-
-import { TransactionDetailsForUserOp } from './TransactionDetailsForUserOp'
-import { resolveProperties } from 'ethers/lib/utils'
-import { PaymasterAPI } from './PaymasterAPI'
-import { getUserOpHash, NotPromise, packUserOp } from '@account-abstraction/utils'
-import { calcPreVerificationGas, GasOverheads } from './calcPreVerificationGas'
-
-
-// const firebase=require('firebase')
-// const firebaseConfig = {
-//   apiKey: "AIzaSyDidOyxwsM4THzhtCACjApprDh5gXfJJOU",
-//   authDomain: "wallet-sdk.firebaseapp.com",
-//   projectId: "wallet-sdk",
-//   storageBucket: "wallet-sdk.appspot.com",
-//   messagingSenderId: "98633293336",
-//   appId: "1:98633293336:web:4e460501defaaa7475ad6d",
-//   measurementId: "G-VBNQR1DWYY"
-// };
-// firebase.initializeApp(firebaseConfig);
-
-// const db = firebase.firestore();
-
-// db.collection('immuna-userOps').doc('hashhashashash').set({
-//   hello:"dsfsdfdsf"
-// })
-
-
-
+const Common = require("ethereumjs-common")
 const Web3 = require('web3')
 const Tx = require('ethereumjs-tx').Transaction
 
 const web3 = new Web3(new Web3.providers.HttpProvider('http://127.0.0.1:8545'))
 
-let tokenAddress = '0xba3D9687Cf50fE253cd2e1cFeEdE1d6787344Ed5' // HST contract address
-let toAddress = '0x8626f6940E2eb28930eFb4CeF49B2d1F2C9C1199' // where to send it
-let fromAddress = '0x71bE63f3384f5fb98995898A86B02Fb2426c5788' // your wallet
-let privateKey = Buffer.from('701b615bbdfb9de65240bc28bd21bbc0d996645a3dd57e7b12bc2bdf6f192c82', 'hex')
-
-let contractABI = [
-  // transfer
-  {
-    'constant': false,
-    'inputs': [
-      {
-        'name': '_to',
-        'type': 'address'
-      },
-      {
-        'name': '_value',
-        'type': 'uint256'
-      }
-    ],
-    'name': 'transfer',
-    'outputs': [
-      {
-        'name': '',
-        'type': 'bool'
-      }
-    ],
-    'type': 'function'
-  }
-]
-const Common = require("ethereumjs-common")
-
-
-let contract = new web3.eth.Contract(contractABI, tokenAddress, {from: fromAddress})
-
-// 1e18 === 1 HST
-let amount = web3.utils.toHex(1e18)
-let chainId=web3.utils.toHex(31337).toString()
-
-const customCommon = Common.default.forCustomChain(
-  'mainnet',
-  {
-    name : "yourNetwork",
-    chainId: chainId,
-    networkId : chainId
-  },
-  "petersburg",
-)
-
-
-// let rawTransaction = {
-//   'from': fromAddress,
-//   'gasPrice': web3.utils.toHex(20 * 1e9),
-//   'gasLimit': web3.utils.toHex(210000),
-//   'to': tokenAddress,
-//   'value': 0x0,
-//   'data': contract.methods.transfer(toAddress, amount).encodeABI(),
-//   'nonce': '',
-//   // 'chainId':chainId
-// }
-// console.log(chainId)
-// web3.eth.getTransactionCount(fromAddress)
-//   .then((count:any) => {
-//     console.log(count)
-//     rawTransaction.nonce=web3.utils.toHex(count)
-
-//     let transaction = new Tx(rawTransaction, {common:customCommon})
-//     transaction.sign(privateKey)
-
-//     console.log(transaction)
-//     web3.eth.sendSignedTransaction('0x' + transaction.serialize().toString('hex'))
-//       .on('transactionHash', console.log)
-
-// })
-
-
-
-
-const getUnsignedERC20AuthTx = async (from: string, controllerAddress: string, erc20Addr: string, amount: number, type: string) => {
+const getUnsignedAuthTx = async (from: string, controllerAddress: string, erc20Addr: string, amount: number, type: string) => {
   if (type === 'aave') {
       let contractABI = [
           // transfer
@@ -173,8 +62,7 @@ const getUnsignedERC20AuthTx = async (from: string, controllerAddress: string, e
   }
 
 }
-const getSignedERC20AuthTx = (rawTx: any, privateKey: any, chainId:number) => {
-  // chainId=web3.utils.toHex(chainId).toString()
+const getSignedAuthTx = (rawTx: any, privateKey: any, chainId:number) => {
   chainId=web3.utils.toHex(31337).toString()
   const customCommon = Common.default.forCustomChain(
       'mainnet',
@@ -185,7 +73,6 @@ const getSignedERC20AuthTx = (rawTx: any, privateKey: any, chainId:number) => {
       },
       "petersburg",
     )
-  // let transaction = new Tx(rawTx, {common:customCommon})
 
   const tx = new Tx(rawTx,{common:customCommon})
   tx.sign(privateKey)
@@ -205,18 +92,20 @@ const deployEthTx = (signedTx: any) => { //forward to rpc
 
 const main=async ()=>{
   const from = '0x71bE63f3384f5fb98995898A86B02Fb2426c5788' //eoa address
-  // const controllerAddr = await api.getAccountAddress() // our controller address
-  const erc20Addr = "0xba3D9687Cf50fE253cd2e1cFeEdE1d6787344Ed5" //interest bearing token
-  const unsignedAuthTx = await getUnsignedERC20AuthTx(from, "0x8626f6940E2eb28930eFb4CeF49B2d1F2C9C1199", erc20Addr, 1e18, 'aave')
-  // const authTxSignature = Buffer.from(
-  //     'ea6c44ac03bff858b476bba40716402b03e41b8e97e276d1baec7c37d42484a0',
-  //     'hex',
-  //   ) // get from user
-    let privateKey = Buffer.from('701b615bbdfb9de65240bc28bd21bbc0d996645a3dd57e7b12bc2bdf6f192c82', 'hex')
+  const erc20Addr = "0xba3D9687Cf50fE253cd2e1cFeEdE1d6787344Ed5" //aave interest bearing token
+  const unsignedAuthTx = await getUnsignedAuthTx(from, "0x8626f6940E2eb28930eFb4CeF49B2d1F2C9C1199", erc20Addr, 1e18, 'aave')
+
+  let privateKey = Buffer.from('701b615bbdfb9de65240bc28bd21bbc0d996645a3dd57e7b12bc2bdf6f192c82', 'hex')
   
-  const signedAuthTx = await getSignedERC20AuthTx(unsignedAuthTx, privateKey,31337)
+  const signedAuthTx = await getSignedAuthTx(unsignedAuthTx, privateKey,31337)
   // console.log(signedAuthTx)
   await deployEthTx(signedAuthTx)
 }
-
 main()
+
+export{
+  deployEthTx,
+  getUnsignedAuthTx,
+  getSignedAuthTx,
+
+}
--- sdk/interaction/test/ImmunaTest.ts
+++ sdk/interaction/test/ImmunaTest.ts
@@ -5,7 +5,7 @@ import {
     UserOperationStruct
 } from '@account-abstraction/contracts'
 import { BigNumber, Wallet } from 'ethers'
-import { getAddress, parseEther } from 'ethers/lib/utils'
+import { getAddress, parseEther, resolveProperties } from 'ethers/lib/utils'
 import { expect } from 'chai'
 const { ethers } = require("hardhat");
 import { SampleRecipient, SampleRecipient__factory } from '@account-abstraction/utils/dist/src/types'
@@ -18,6 +18,19 @@ const treasuryAbi = require("../../../web3/build/contracts/Treasury.json").abi
 const firebase = require('firebase')
 import { TreasuryAPI } from '../src/TreasuryAPI'
 import { BaseAccountAPI } from '../src/BaseAccountAPI'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDidOyxwsM4THzhtCACjApprDh5gXfJJOU",
+    authDomain: "wallet-sdk.firebaseapp.com",
+    projectId: "wallet-sdk",
+    storageBucket: "wallet-sdk.appspot.com",
+    messagingSenderId: "98633293336",
+    appId: "1:98633293336:web:4e460501defaaa7475ad6d",
+    measurementId: "G-VBNQR1DWYY"
+};
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
 
 
 let owner: Wallet
@@ -82,36 +95,36 @@ const main = async () => {
 
 
 
-    const getUnsignedERC20AuthTx = async (from: string, controllerAddress: string, erc20Addr: string, amount: number, type: string) => {
+    const getUnsignedAuthTx = async (from: string, controllerAddress: string, erc20Addr: string, amount: number, type: string) => {
         if (type === 'aave') {
             let contractABI = [
                 // transfer
                 {
-                  'constant': false,
-                  'inputs': [
-                    {
-                      'name': '_to',
-                      'type': 'address'
-                    },
-                    {
-                      'name': '_value',
-                      'type': 'uint256'
-                    }
-                  ],
-                  'name': 'transfer',
-                  'outputs': [
-                    {
-                      'name': '',
-                      'type': 'bool'
-                    }
-                  ],
-                  'type': 'function'
+                    'constant': false,
+                    'inputs': [
+                        {
+                            'name': '_to',
+                            'type': 'address'
+                        },
+                        {
+                            'name': '_value',
+                            'type': 'uint256'
+                        }
+                    ],
+                    'name': 'transfer',
+                    'outputs': [
+                        {
+                            'name': '',
+                            'type': 'bool'
+                        }
+                    ],
+                    'type': 'function'
                 }
-              ]
-            
-            
-            let myContract = new web3.eth.Contract(contractABI, erc20Addr, {from:from});
-            amount = web3.utils.toHex(amount) 
+            ]
+
+
+            let myContract = new web3.eth.Contract(contractABI, erc20Addr, { from: from });
+            amount = web3.utils.toHex(amount)
 
             let nonce = await web3.eth.getTransactionCount(from)
             // let nonce=web3.utils.toHex(9)
@@ -125,9 +138,9 @@ const main = async () => {
                 'data': myContract.methods.transfer(controllerAddress, amount).encodeABI(),
                 'nonce': web3.utils.toHex(nonce),
                 // 'chainId':chainId
-              }
-            
-            
+            }
+
+
             return rawTransaction
         }
         else if (type === 'uniswap') {
@@ -139,28 +152,28 @@ const main = async () => {
         }
 
     }
-    const getSignedERC20AuthTx = (rawTx: any, privateKey: any, chainId:number) => {
+    const getSignedAuthTx = (rawTx: any, privateKey: any, chainId: number) => {
         // chainId=web3.utils.toHex(chainId).toString()
-        chainId=web3.utils.toHex(31337).toString()
+        chainId = web3.utils.toHex(31337).toString()
         const customCommon = Common.default.forCustomChain(
             'mainnet',
             {
-              name : "yourNetwork",
-              chainId: chainId,
-              networkId : chainId
+                name: "yourNetwork",
+                chainId: chainId,
+                networkId: chainId
             },
             "petersburg",
-          )
+        )
         // let transaction = new Tx(rawTx, {common:customCommon})
 
-        const tx = new Tx(rawTx,{common:customCommon})
+        const tx = new Tx(rawTx, { common: customCommon })
         tx.sign(privateKey)
         // console.log(tx)
         let serializedTx = '0x' + tx.serialize().toString('hex');
         return serializedTx
     }
     const deployEthTx = async (signedTx: any) => { //forward to rpc
-        
+
 
         web3.eth.sendSignedTransaction(signedTx).on('transactionHash', function (txHash: any) {
             console.log(txHash)
@@ -180,6 +193,7 @@ const main = async () => {
     }
 
     const executeUserOp = async (userOp: UserOperationStruct) => {
+
         const EntryPointFactory = await ethers.getContractFactory('EntryPoint')
         const entryPoint = await EntryPointFactory.deploy()
         const config = {
@@ -260,7 +274,7 @@ const main = async () => {
         let addr = accountApi.getAddressFromType(type)
 
         const accountAddress = await accountApi.getAccountAddress()
-        
+
 
         await web3.eth.sendTransaction({ to: accountAddress, from: accs[0], value: web3.utils.toWei("10", "ether") })
 
@@ -299,14 +313,29 @@ const main = async () => {
         op.initCode = "1"
         return op;
     }
-    const storeUserOp = (userOp: UserOperationStruct) => {
-
+    const storeUserOp = async (userOp: UserOperationStruct) => {
+        const userOp1=await resolveProperties(userOp)
+        const userOpHash=getUserOpHash(userOp1,entryPointAddress,5)
+        db.collection('immuna-userOps').doc(userOpHash).set({
+            userOp
+        }).then(()=>{
+            return true;
+        }).catch(()=>{
+            return false;
+        })
     }
-
+    const getUserOp=async (userOpHash:string)=>{
+        let ref=await db.collection('immuna-userOps').doc(userOpHash)
+        ref.get().then((doc:any)=>{
+            return doc.data()
+        }).catch(()=>{
+            throw new Error('doc not found')
+        })
+    }
     const from = '0x71bE63f3384f5fb98995898A86B02Fb2426c5788' //eoa address
     const controllerAddr = await api.getAccountAddress() // our controller address
     const erc20Addr = "0xba3D9687Cf50fE253cd2e1cFeEdE1d6787344Ed5" //interest bearing token
-    const unsignedAuthTx = await getUnsignedERC20AuthTx(from, controllerAddr, erc20Addr, 1e18, 'aave')
+    const unsignedAuthTx = await getUnsignedAuthTx(from, controllerAddr, erc20Addr, 1e18, 'aave')
     console.log(controllerAddr)
     // const authTxSignature = Buffer.from(
     //     'ea6c44ac03bff858b476bba40716402b03e41b8e97e276d1baec7c37d42484a0',
@@ -314,18 +343,18 @@ const main = async () => {
     //   ) // get from user
     let privateKey = Buffer.from('701b615bbdfb9de65240bc28bd21bbc0d996645a3dd57e7b12bc2bdf6f192c82', 'hex')
 
-    const signedAuthTx = await getSignedERC20AuthTx(unsignedAuthTx, privateKey,31337)
-    await deployEthTx(signedAuthTx)
+    const signedAuthTx = await getSignedAuthTx(unsignedAuthTx, privateKey, 31337)
+    // await deployEthTx(signedAuthTx)
 
-    // const unsignedUserOp = await getUnsignedUserOp(api,
-    //     'aave',
-    //     amountToSend,
-    //     rpcURL
-    // )
-    // const userOpSignature = "." // TODO
-    // const signedUserOp = await getSignedUserOp(api, unsignedUserOp, userOpSignature)
-    // storeUserOp(signedUserOp)
-    // executeUserOp(signedUserOp)
+    const unsignedUserOp = await getUnsignedUserOp(api,
+        'aave',
+        amountToSend,
+        rpcURL
+    )
+    const userOpSignature = "." // TODO
+    const signedUserOp = await getSignedUserOp(api, unsignedUserOp, userOpSignature)
+    storeUserOp(signedUserOp)
+    executeUserOp(signedUserOp)
 
 
     // const walletSpec = await getWalletSpec("Aave")
@@ -349,51 +378,51 @@ const main = async () => {
 
 // const getWalletSpec = async (type: string): Promise<UserOperationStruct> => {
 
-    //     entryPoint = await new EntryPoint__factory(signer).deploy()
-    //     beneficiary = await signer.getAddress()
+//     entryPoint = await new EntryPoint__factory(signer).deploy()
+//     beneficiary = await signer.getAddress()
 
-    //     recipient = await new SampleRecipient__factory(signer).deploy()
-    //     owner = Wallet.createRandom()
-    //     const factoryAddress = await DeterministicDeployer.deploy(TreasuryFactory__factory.bytecode)
-    //     //create userop
-    //     let wallet:any;
-    //     if (type === 'aave' || type==='uniswap') {
-    //         wallet=new TreasuryAPI({
-    //             provider,
-    //         entryPointAddress: entryPoint.address,
-    //         owner,
-    //         factoryAddress
-    //         })
-    //     }
-    //     else {
-    //         throw new Error(`${type} of wallet is not available`)
-    //         return null
-    //     }
+//     recipient = await new SampleRecipient__factory(signer).deploy()
+//     owner = Wallet.createRandom()
+//     const factoryAddress = await DeterministicDeployer.deploy(TreasuryFactory__factory.bytecode)
+//     //create userop
+//     let wallet:any;
+//     if (type === 'aave' || type==='uniswap') {
+//         wallet=new TreasuryAPI({
+//             provider,
+//         entryPointAddress: entryPoint.address,
+//         owner,
+//         factoryAddress
+//         })
+//     }
+//     else {
+//         throw new Error(`${type} of wallet is not available`)
+//         return null
+//     }
 
-    //     const unsignedUserOp = await wallet.encode({
-    //         type,
-    //         amountToSend,
-    //         'http://127.0.0.1:8545'
-    //     })
-    //     return unsignedUserOp
+//     const unsignedUserOp = await wallet.encode({
+//         type,
+//         amountToSend,
+//         'http://127.0.0.1:8545'
+//     })
+//     return unsignedUserOp
 
-    // }
+// }
 
 
-     // it('test', async () => {
-        // const contractAbi: any[] = []
-        // const controllerAddr = "0xfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff" // oour controller address
-        // const erc20Addr = "0xfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff" // address of their token
-        // const authTxSignature = "." // TODO
-        // const signedAuthTx = getSignedERC20AuthTx(unsignedAuthTx, authTxSignature)
-        // const providerURL = ""
+// it('test', async () => {
+// const contractAbi: any[] = []
+// const controllerAddr = "0xfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff" // oour controller address
+// const erc20Addr = "0xfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff" // address of their token
+// const authTxSignature = "." // TODO
+// const signedAuthTx = getSignedERC20AuthTx(unsignedAuthTx, authTxSignature)
+// const providerURL = ""
 
-        // const unsignedUserOp = await api.getUnsignedUserOp(
-        //     'aave',
-        //     amountToSend,
-        //     'http://127.0.0.1:8545'
-        // )
-        // notifyBundlerOfEvent(userOpSignature)
-    // })
+// const unsignedUserOp = await api.getUnsignedUserOp(
+//     'aave',
+//     amountToSend,
+//     'http://127.0.0.1:8545'
+// )
+// notifyBundlerOfEvent(userOpSignature)
+// })
 
 main()
