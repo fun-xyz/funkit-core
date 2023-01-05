89031000f75faf4ff699d4cb131f4d1bae629e03
Caleb Sirak <calebsirak@gmail.com>
2023-01-04 17:58:34 -0800

    updated calling of actions

--- sdk/hardcode/AaveOp.js
+++ sdk/hardcode/AaveOp.js
@@ -1,12 +1,6 @@
 const ethers = require('ethers')
-const { Contract, Signer, Wallet } = ethers
-
-const { ERC4337EthersProvider, ERC4337EthersSigner, ClientConfig, wrapProvider, HttpRpcClient, SimpleAccountAPI, DeterministicDeployer } = require('@account-abstraction/sdk')
-const contracts = require('@account-abstraction/contracts')
-const { SimpleAccount__factory } = contracts.factories
-const { parseEther } = require('ethers/lib/utils')
-
-
+const { createHash } = require("crypto")
+const { wrapProvider, HttpRpcClient, DeterministicDeployer } = require('@account-abstraction/sdk')
 const { TreasuryAPI } = require("./treasuryapi")
 
 const treasuryAbi = require("../../web3/build/contracts/Treasury.json").abi
@@ -16,15 +10,16 @@ const entrypointcontract = require("../../web3/build/contracts/EntryPoint.json")
 
 const url = "http://localhost:8545"
 
-const provider = new ethers.providers.JsonRpcProvider(url);
 
 const Web3 = require('web3')
 const web3 = new Web3(url);
 
 const factoryAddress = "0xcc783140446C68edC7B3C601E63665E17e232eE8"
-const aaveActionAddr = "0x4026F10CAB74300fC2AF54215532d0b75088FcE2"
 const entryPointAddress = "0x1306b01bC3e4AD202612D3843387e94737673F53"
+const aaveActionAddr = "0x4026F10CAB74300fC2AF54215532d0b75088FcE2"
 const key = "key1"
+
+
 
 
 const getEntryPointLogs = async () => {
@@ -35,10 +30,9 @@ const getEntryPointLogs = async () => {
 
 }
 const main = async () => {
-
     const aavactioncontract = await new web3.eth.Contract(actionContract, aaveActionAddr);
 
-    const getTestData = async () => {
+    const getTestData = async (key) => {
         const data = await aavactioncontract.methods.requests(key).call()
         // const out = web3.eth.abi.decodeParameters(["address", "address", "uint256", "string"], data)
         console.log(data)
@@ -84,17 +78,15 @@ const main = async () => {
     await web3.eth.sendTransaction({ to: accountAddress, from: accs[0], value: web3.utils.toWei("10", "ether") })
 
     const aavedata = web3.eth.abi.encodeParameters(["address", "address", "uint256", "string"], ['0x76ca03a67C049477FfB09694dFeF00416dB69746', '0x39dD11C243Ac4Ac250980FA3AEa016f73C509f37', '10', key]);
-    const aavecall = aavactioncontract.methods.init(aavedata)
+
+    createAAVETrackingPosition
 
     // const aavedata2 = web3.eth.abi.encodeParameters(["address", "address", "uint256", "string"], ['0x76ca03a67C049477FfB09694dFeF00416dB69746', '0x39dD11C243Ac4Ac250980FA3AEa016f73C509f37', '10', "key2"]);
     // const aavecall2 = aavactioncontract.methods.init(aavedata2)
     // await web3.eth.sendTransaction({ to: aaveActionAddr, from: accs[0], data: aavecall2.encodeABI() })
 
     try {
-        const op = await accountApi.createSignedUserOp({
-            target: aaveActionAddr,
-            data: aavecall.encodeABI()
-        })
+
         // console.log(op)
         // await sendTestOP(accountAddress, encodeCallOp(aaveActionAddr, aavecall.encodeABI()))
         // await sendTestOP(aaveActionAddr, aavecall.encodeABI())
@@ -144,5 +136,154 @@ const encodeCallOp = (addr, data, value = "0") => {
 }
 
 
-main()
 
+class FunWallet {
+    MAX_INT = ethers.BigNumber.from("0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff");
+    sha256(content) {
+        return createHash('sha256').update(content).digest('hex')
+    }
+
+    constructor(rpcURL, bundlerUrl, entryPointAddress, ownerAccount, factoryAddress) {
+        this.provider = new ethers.providers.JsonRpcProvider(rpcURL);
+        this.config = { bundlerUrl, entryPointAddress }
+        this.bundlerUrl = bundlerUrl
+        this.entryPointAddress = entryPointAddress
+        this.ownerAccount = ownerAccount
+        this.factoryAddress = factoryAddress
+    }
+    async init() {
+        this.erc4337Provider = await wrapProvider(
+            this.provider,
+            this.config,
+            this.ownerAccount
+        )
+
+        await DeterministicDeployer.init(this.provider)
+
+        const net = await this.erc4337Provider.getNetwork()
+        const accs = await web3.eth.getAccounts()
+        this.rpcClient = new HttpRpcClient(this.bundlerUrl, this.entryPointAddress, net.chainId)
+
+        this.accountApi = new TreasuryAPI({
+            provider: this.erc4337Provider,
+            entryPointAddress: this.entryPointAddress,  //check this
+            owner: this.ownerAccount,
+            factoryAddress: this.factoryAddress,
+            // index: 3
+        })
+
+        this.accountAddress = await this.accountApi.getAccountAddress()
+        return this.accountAddress
+    }
+
+    async sendOpToBundler(op) {
+        const userOpHash = await this.rpcClient.sendUserOpToBundler(op)
+        const txid = await this.accountApi.getUserOpReceipt(userOpHash)
+        return { userOpHash, txid }
+    }
+
+    async getPreAAVEtransactions(aTokenAddr, from, amount = MAX_INT) {
+        // approve
+        let contractABI = [
+            {
+                "inputs": [
+                    {
+                        "internalType": "address",
+                        "name": "spender",
+                        "type": "address"
+                    },
+                    {
+                        "internalType": "uint256",
+                        "name": "amount",
+                        "type": "uint256"
+                    }
+                ],
+                "name": "approve",
+                "outputs": [
+                    {
+                        "internalType": "bool",
+                        "name": "",
+                        "type": "bool"
+                    }
+                ],
+                "stateMutability": "nonpayable",
+                "type": "function"
+            },
+        ]
+
+
+        const atokenContract = new web3.eth.Contract(contractABI, aTokenAddr, { from });
+        amount = web3.utils.toHex(amount)
+        let nonce = await web3.eth.getTransactionCount(from)
+        let rawTransaction = {
+            'from': from,
+            'gasPrice': web3.utils.toHex(20 * 1e9),
+            'gasLimit': web3.utils.toHex(210000),
+            'to': aTokenAddr,
+            'data': atokenContract.methods.approve(controllerAddress, amount).encodeABI(),
+            'nonce': web3.utils.toHex(nonce),
+        }
+        return rawTransaction
+    }
+
+    async createAAVETrackingPosition(actionAddr, userAddr, aTokenAddr, positionMax, storageKey = "") {
+        const aavactioncontract = await new web3.eth.Contract(actionContract);
+        const hashinput = [userAddr, aTokenAddr, positionMax, storageKey].toString()
+        const key = sha256(hashinput)
+        const aavedata = web3.eth.abi.encodeParameters(["address", "address", "uint256", "string"], [userAddr, aTokenAddr, positionMax, key]);
+        const aavecall = aavactioncontract.methods.init(aavedata)
+        const op = await this.accountApi.createSignedUserOp({
+            target: actionAddr,
+            data: aavecall.encodeABI()
+        })
+        return { op, key }
+    }
+
+    async executeAAVETrackingPosition(actionAddr, storageKey) {
+        const aavactioncontract = await new web3.eth.Contract(actionContract);
+        const aavedata = web3.eth.abi.encodeParameters(["string"], [storageKey]);
+        const aavecall = aavactioncontract.methods.execute(aavedata)
+        const op = await this.accountApi.createSignedUserOp({
+            target: actionAddr,
+            data: aavecall.encodeABI()
+        })
+        return { op }
+    }
+}
+
+const getTestData = async (address, key) => {
+    const aavactioncontract = await new web3.eth.Contract(actionContract, address);
+    const data = await aavactioncontract.methods.requests(key).call()
+    const out = web3.eth.abi.decodeParameters(["address", "address", "uint256", "string"], data)
+    console.log(out)
+}
+
+const aaveCreateTest = async (wallet) => {
+    //create and send user op
+    let { op, key } = await wallet.createAAVETrackingPosition(aaveActionAddr, '0x76ca03a67C049477FfB09694dFeF00416dB69746', '0x39dD11C243Ac4Ac250980FA3AEa016f73C509f37', '10')
+    const receipt = await wallet.sendOpToBundler(op)
+    // getTestData(aaveActionAddr, key)
+    return key
+}
+
+const aaveExecuteTest = async (wallet, key) => {
+    let { op } = await wallet.executeAAVETrackingPosition(aaveActionAddr, key)
+    const receipt = await wallet.sendOpToBundler(op)
+    console.log(receipt)
+}
+
+const aaveTest = async () => {
+    const ownerAccount = new ethers.Wallet(privateKey = "0x4bbbf85ce3377467afe5d46f804f221813b2bb87f24d81f60f1fcdbf7cbf4356")
+    // initialize wallet
+    const wallet = new FunWallet(url, 'http://localhost:3000/rpc', entryPointAddress, ownerAccount, factoryAddress)
+    const address = await wallet.init()
+    //fund wallet
+    const accs = await web3.eth.getAccounts()
+    await web3.eth.sendTransaction({ to: address, from: accs[0], value: web3.utils.toWei("10", "ether") })
+
+    const key = await aaveCreateTest(wallet)
+    await aaveExecuteTest(wallet, key)
+}
+
+
+aaveTest()
