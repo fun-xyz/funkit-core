146a7d555d2cd047537e717a1f25ff22aa719901
Caleb Sirak <calebsirak@gmail.com>
2023-01-07 05:19:35 -0800

    completed V1

--- packages/interaction/dist/hardhat.config.d.ts
+++
@@ -1,5 +0,0 @@
-import '@nomiclabs/hardhat-ethers';
-import '@nomicfoundation/hardhat-toolbox';
-import { HardhatUserConfig } from 'hardhat/config';
-declare const config: HardhatUserConfig;
-export default config;
--- tests/TestAAVEPosition.js
+++ tests/TestAAVEPosition.js
@@ -3,17 +3,15 @@ const ethers = require('ethers')
 
 const main = async () => {
     const ATokenAddress = "0xC42f40B7E22bcca66B3EE22F3ACb86d24C997CC2" // Avalanche Fuji AAVE Dai
-
-
     const eoa = new ethers.Wallet(privateKey = "0x66f37ee92a08eebb5da72886f3c1280d5d1bd5eb8039f52fdb8062df7e364206") // Metamask browser side etc
     const wallet = new FunWallet(eoa)
     const treasuryAddr = await wallet.init()
 
 
     // Fund Wallet
-    // const tx = await wallet.eoa.sendTransaction({ to: treasuryAddr, from: eoa.address, value: ethers.utils.parseEther(".3") })
-    // const fundReceipt = await tx.wait()
-    
+    const tx = await wallet.eoa.sendTransaction({ to: treasuryAddr, from: eoa.address, value: ethers.utils.parseEther(".3") })
+    const fundReceipt = await tx.wait()
+
     const approveTokenTX = await wallet.createTokenApprovalTx(ATokenAddress)
     await wallet.deployTokenApprovalTx(approveTokenTX)
 
