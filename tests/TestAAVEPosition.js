8dcd50a3e19b218d7cd73ce4308874a5a4f67cad
Mario Baxter <mariobaxter@Marios-MacBook-Pro.local>
2023-01-08 16:21:35 -0800

    Added comments to TestAAVEPosition.js

--- tests/TestAAVEPosition.js
+++ tests/TestAAVEPosition.js
@@ -1,31 +1,36 @@
 const { FunWallet } = require("../index")
 const ethers = require('ethers')
 
-const ATokenAddress = "0xC42f40B7E22bcca66B3EE22F3ACb86d24C997CC2" // Avalanche Fuji AAVE Dai
+const aTokenAddress = "0xC42f40B7E22bcca66B3EE22F3ACb86d24C997CC2" // Avalanche Fuji AAVE Dai
 const privKey = "c5ff68eee74d447b88d5d0dd1d438b37f30a4c0b1e41e9c485c6e2ea282d1488"
 
 const main = async () => {
     // Create an EOA instance with ethers
     const eoa = new ethers.Wallet(privKey)
 
-    // Create a new FunWallet instance, initially funded with 0.3 AVAX to cover gas fees
-    const wallet = new FunWallet(eoa, ".3")
+    // Create a new FunWallet instance, 
+    const wallet = new FunWallet()
 
-    // Initialize the FunWallet instance
-    await wallet.init()
+    // Initialize the FunWallet instance, initially funded with 0.3 AVAX to cover gas fees
+    await wallet.init(eoa, ".3")
 
     // Add the withdraw from aave action to the FunWallet
-    await wallet.addAction("AAVE", ATokenAddress)
+    await wallet.addAction("AAVE", aTokenAddress)
 
     // Deploy the FunWallet with the withdraw from Aave action
     const { receipt: deplomentReceipt, executionHash } = await wallet.deployWallet()
     console.log("Creation Succesful:\n", deplomentReceipt)    
 
-    // Deploy a transaction approving the FunWallet to move the aave tokens from the EOA
+    /* 
+    Deploy a transaction approving the FunWallet to move the aave tokens from the EOA to the
+    Aave smart contract.
+    
+    */
     const approveReceipt = await wallet.deployTokenApprovalTx()
     console.log("Approval Succesful:\n", approveReceipt)
 
     // after some time
+    // 
     const executionReceipt = await wallet.executeAction(executionHash)
     console.log("Execution Succesful:\n", executionReceipt)
 
