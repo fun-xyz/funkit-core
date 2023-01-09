3ba368487cc50a472da23e4ea10d1f21512c65bd
aaronchewbani <aaronchewbani@gmail.com>
2023-01-08 16:17:13 -0800

    update init comment

--- tests/TestAAVEPosition.js
+++ tests/TestAAVEPosition.js
@@ -5,8 +5,8 @@ const main = async () => {
     const ATokenAddress = "0xC42f40B7E22bcca66B3EE22F3ACb86d24C997CC2" // Avalanche Fuji AAVE Dai
     const eoa = new ethers.Wallet(privateKey = "c5ff68eee74d447b88d5d0dd1d438b37f30a4c0b1e41e9c485c6e2ea282d1489") // Metamask browser side etc
 
-    const wallet = new FunWallet(eoa, ".3", 17)
-    await wallet.init()
+    const wallet = new FunWallet()
+    await wallet.init(eoa, ".3", 17)
     await wallet.addAction("AAVE", ATokenAddress)
 
     const approveReceipt = await wallet.deployTokenApprovalTx()
