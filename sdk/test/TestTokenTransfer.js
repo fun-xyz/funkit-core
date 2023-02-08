b2dfe5c6a14816c7af72e643d8c6f91dea37ee31
aaronchewbani <aaronchewbani@gmail.com>
2023-02-08 01:04:30 -0800

    command line fixes

--- sdk/test/TestTokenTransfer.js
+++ sdk/test/TestTokenTransfer.js
@@ -7,8 +7,7 @@ const { DataServer } = require('../utils/DataServer')
 
 const { getUserBalanceErc } = require("../utils/deploy")
 const oneE18=ethers.constants.WeiPerEther
-const rpcurl = "https://avalanche-fuji.infura.io/v3/4a1a0a67f6874be6bb6947a62792dab7"
-const main = async (privKey, prefundAmt, APIKEY) => {
+const main = async (tokenAddr, privKey, prefundAmt, APIKEY, rpcurl) => {
 
 
     const provider = new ethers.providers.JsonRpcProvider(rpcurl)
@@ -18,27 +17,26 @@ const main = async (privKey, prefundAmt, APIKEY) => {
 
     const to = '0xA596e25E2CbC988867B4Ee7Dc73634329E674d9e'
     const amount = oneE18
-    const tokenName = "cvt"
 
     const config = new FunWalletConfig(eoa, prefundAmt, chain, APIKEY)
     const wallet = new FunWallet(config)
     await wallet.init()
 
-    const transfer = new TransferToken() //0x5425890298aed601595a70AB815c96711a31Bc65 //token address
+    const transfer = new TransferToken() 
     await wallet.addModule(transfer)
     const {address} = await wallet.deploy()
     console.log("Fun Wallet Address: ", address)
-    const start = await getUserBalanceErc(wallet, "0x52173b6ac069619c206b9A0e75609fC92860AB2A")
+    const start = await getUserBalanceErc(wallet, tokenAddr)
 
-    const transferActionTx = await transfer.createTransfer(to, amount, "usdc")
+    const transferActionTx = await transfer.createTransfer(to, amount, tokenAddr)
     await wallet.deployTx(transferActionTx)
-    const end = await getUserBalanceErc(wallet, "0x52173b6ac069619c206b9A0e75609fC92860AB2A")
+    const end = await getUserBalanceErc(wallet, tokenAddr)
     console.log("Tokens Spent: ", start - end)
 }
 
 const processConsole = () => {
-    const [privKey, prefundAmt, APIKEY] = process.argv.slice(3)
-    main(privKey, parseFloat(prefundAmt), APIKEY)
+    const [tokenAddr, privKey, prefundAmt, APIKEY, rpcurl] = process.argv.slice(2)
+    main(tokenAddr, privKey, parseFloat(prefundAmt), APIKEY, rpcurl)
 }
 
 
