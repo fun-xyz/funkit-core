4161da6430d87097e027cd5f070ff3fdefe45678
Your Name <calebsirak@gmail.com>
2023-02-14 17:16:23 -0800

    updated based on chaz notes

--- test/TestAAVEPosition.js
+++ test/TestAAVEPosition.js
@@ -1,21 +1,116 @@
 const { FunWalletConfig } = require("../src/funWallet")
 const { EoaAaveWithdrawal, ApproveAndSwap, TransferToken } = require("../src/modules/index")
 const { FunWallet } = require("../index")
-const { execTest, transferAmt, getAddrBalanceErc, timeout, getBalance, transferErc, execContractFunc, getUserBalanceErc, createErc, getAllowanceErc, } = require("../utils/deploy")
+const { transferAmt, getAddrBalanceErc, timeout, getBalance, execContractFunc, getUserBalanceErc, createErc, getAllowanceErc, } = require("../utils/deploy")
 
 const ethers = require('ethers')
-const { Token } = require("../utils/Token")
 
-const ABI = ethers.utils.defaultAbiCoder;
+const PRIV_KEY = "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
 
 const CHAIN = 31337 // avax fuji 
 
 const DAI = "0x6B175474E89094C44Da98b954EedeAC495271d0F"
 const aDAI = "0x018008bfb33d285247A21d44E50697654f754e63"
 const routerAddr = "0xE592427A0AEce92De3Edee1F18E0157C05861564"
-
 const POOL_ADDRESS = "0x7d2768dE32b0b80b7a3454c06BdAc94A69DDc7A9"
 
+
+const PREFUND_AMT = 0.3
+
+const APIKEY = "hnHevQR0y394nBprGrvNx4HgoZHUwMet5mXTOBhf"
+const RPC_URL = "http://127.0.0.1:8545"
+
+const TOKEN_ADDRESS = DAI
+const GET_RESERVE_DATA_ABI = [
+    {
+        "inputs": [
+            {
+                "internalType": "address",
+                "name": "asset",
+                "type": "address"
+            }
+        ],
+        "name": "getReserveData",
+        "outputs": [
+            {
+                "components": [
+                    {
+                        "components": [
+                            {
+                                "internalType": "uint256",
+                                "name": "data",
+                                "type": "uint256"
+                            }
+                        ],
+                        "internalType": "struct DataTypes.ReserveConfigurationMap",
+                        "name": "configuration",
+                        "type": "tuple"
+                    },
+                    {
+                        "internalType": "uint128",
+                        "name": "liquidityIndex",
+                        "type": "uint128"
+                    },
+                    {
+                        "internalType": "uint128",
+                        "name": "variableBorrowIndex",
+                        "type": "uint128"
+                    },
+                    {
+                        "internalType": "uint128",
+                        "name": "currentLiquidityRate",
+                        "type": "uint128"
+                    },
+                    {
+                        "internalType": "uint128",
+                        "name": "currentVariableBorrowRate",
+                        "type": "uint128"
+                    },
+                    {
+                        "internalType": "uint128",
+                        "name": "currentStableBorrowRate",
+                        "type": "uint128"
+                    },
+                    {
+                        "internalType": "uint40",
+                        "name": "lastUpdateTimestamp",
+                        "type": "uint40"
+                    },
+                    {
+                        "internalType": "address",
+                        "name": "aTokenAddress",
+                        "type": "address"
+                    },
+                    {
+                        "internalType": "address",
+                        "name": "stableDebtTokenAddress",
+                        "type": "address"
+                    },
+                    {
+                        "internalType": "address",
+                        "name": "variableDebtTokenAddress",
+                        "type": "address"
+                    },
+                    {
+                        "internalType": "address",
+                        "name": "interestRateStrategyAddress",
+                        "type": "address"
+                    },
+                    {
+                        "internalType": "uint8",
+                        "name": "id",
+                        "type": "uint8"
+                    }
+                ],
+                "internalType": "struct DataTypes.ReserveData",
+                "name": "",
+                "type": "tuple"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+]
 const supplyAbi = [{
     "inputs": [
         {
@@ -44,6 +139,22 @@ const supplyAbi = [{
     "stateMutability": "nonpayable",
     "type": "function"
 }]
+
+
+const UNDERLYING_ASSET_ABI = [{
+    "inputs": [],
+    "name": "UNDERLYING_ASSET_ADDRESS",
+    "outputs": [
+        {
+            "internalType": "address",
+            "name": "",
+            "type": "address"
+        }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+},]
+
 
 const walletTransferERC = async (wallet, to, amount, tokenAddr) => {
     const transfer = new TransferToken()
@@ -74,6 +185,16 @@ const walletEthToERC20Swap = async (wallet, eoa, amount, tokenAddr, returnAddres
     console.log("Wallet Eth End Balance: ", await getBalance(wallet))
 }
 
+const getAtokenAddress = async (eoa, tokenAddress) => {
+    const poolIface = new ethers.Contract(POOL_ADDRESS, GET_RESERVE_DATA_ABI, eoa)
+    return await poolIface.getReserveData(tokenAddress)
+}
+
+const getUnderlyingAsset = async (eoa, atokenAddress) => {
+    const atokenIface = new ethers.Contract(atokenAddress, UNDERLYING_ASSET_ABI, eoa)
+    return await atokenIface.UNDERLYING_ASSET_ADDRESS()
+}
+
 const eoaSupplyAave = async (eoa, amount, tokenAddr) => {
     const tokenContract = createErc(tokenAddr, eoa)
     const approveData = await tokenContract.populateTransaction.approve(POOL_ADDRESS, amount)
@@ -95,7 +216,7 @@ const setUpWithdrawEOA = async (eoa, wallet, amount, tokenAddr) => {
 }
 
 const mainTest = async (wallet, tokenAddr) => {
-    // Create a FunWallet with the above access control schema, prefunded with prefundAmt AVAXa
+    // Create a FunWallet with the above access control schema, prefunded with PREFUND_AMT AVAXa
     const module = new EoaAaveWithdrawal()
 
     await wallet.addModule(module)
@@ -114,138 +235,27 @@ const mainTest = async (wallet, tokenAddr) => {
 }
 
 
-const getAtokenAddress = async (eoa, tokenAddress) => {
-    const abi = [
-        {
-            "inputs": [
-                {
-                    "internalType": "address",
-                    "name": "asset",
-                    "type": "address"
-                }
-            ],
-            "name": "getReserveData",
-            "outputs": [
-                {
-                    "components": [
-                        {
-                            "components": [
-                                {
-                                    "internalType": "uint256",
-                                    "name": "data",
-                                    "type": "uint256"
-                                }
-                            ],
-                            "internalType": "struct DataTypes.ReserveConfigurationMap",
-                            "name": "configuration",
-                            "type": "tuple"
-                        },
-                        {
-                            "internalType": "uint128",
-                            "name": "liquidityIndex",
-                            "type": "uint128"
-                        },
-                        {
-                            "internalType": "uint128",
-                            "name": "variableBorrowIndex",
-                            "type": "uint128"
-                        },
-                        {
-                            "internalType": "uint128",
-                            "name": "currentLiquidityRate",
-                            "type": "uint128"
-                        },
-                        {
-                            "internalType": "uint128",
-                            "name": "currentVariableBorrowRate",
-                            "type": "uint128"
-                        },
-                        {
-                            "internalType": "uint128",
-                            "name": "currentStableBorrowRate",
-                            "type": "uint128"
-                        },
-                        {
-                            "internalType": "uint40",
-                            "name": "lastUpdateTimestamp",
-                            "type": "uint40"
-                        },
-                        {
-                            "internalType": "address",
-                            "name": "aTokenAddress",
-                            "type": "address"
-                        },
-                        {
-                            "internalType": "address",
-                            "name": "stableDebtTokenAddress",
-                            "type": "address"
-                        },
-                        {
-                            "internalType": "address",
-                            "name": "variableDebtTokenAddress",
-                            "type": "address"
-                        },
-                        {
-                            "internalType": "address",
-                            "name": "interestRateStrategyAddress",
-                            "type": "address"
-                        },
-                        {
-                            "internalType": "uint8",
-                            "name": "id",
-                            "type": "uint8"
-                        }
-                    ],
-                    "internalType": "struct DataTypes.ReserveData",
-                    "name": "",
-                    "type": "tuple"
-                }
-            ],
-            "stateMutability": "view",
-            "type": "function"
-        },
-    ]
-    const poolIface = new ethers.Contract(POOL_ADDRESS, abi, eoa)
-    return await poolIface.getReserveData(tokenAddress)
-}
 
-const getUnderlyingAsset = async (eoa, atokenAddress) => {
-    const abi = [{
-        "inputs": [],
-        "name": "UNDERLYING_ASSET_ADDRESS",
-        "outputs": [
-            {
-                "internalType": "address",
-                "name": "",
-                "type": "address"
-            }
-        ],
-        "stateMutability": "view",
-        "type": "function"
-    },]
-    const atokenIface = new ethers.Contract(atokenAddress, abi, eoa)
-    return await atokenIface.UNDERLYING_ASSET_ADDRESS()
-}
 const amount = 10;
-const main = async (privKey, prefundAmt, APIKEY, rpcurl) => {
-    const provider = new ethers.providers.JsonRpcProvider(rpcurl)
-    const eoa = new ethers.Wallet(privKey, provider)
-    const walletConfig = new FunWalletConfig(eoa, CHAIN, APIKEY, prefundAmt)
+const main = async (PRIV_KEY, PREFUND_AMT, APIKEY, RPC_URL) => {
+    const provider = new ethers.providers.JsonRpcProvider(RPC_URL)
+    const eoa = new ethers.Wallet(PRIV_KEY, provider)
+    const walletConfig = new FunWalletConfig(eoa, CHAIN, APIKEY, PREFUND_AMT)
     const wallet = new FunWallet(walletConfig)
     await wallet.init()
-    await setUpWithdrawEOA(eoa, wallet, amount, tokenAddr)
+    await setUpWithdrawEOA(eoa, wallet, amount, TOKEN_ADDRESS)
 
     console.log("Waiting for block to pass.")
     await timeout(4000)
 
-    const { aTokenAddress } = await getAtokenAddress(eoa, tokenAddr)
+    const { aTokenAddress } = await getAtokenAddress(eoa, TOKEN_ADDRESS)
     const baseTokenAddress = await getUnderlyingAsset(eoa, aTokenAddress)
     console.log(baseTokenAddress)
-    const eoaTokenBalance = await getAddrBalanceErc(eoa, tokenAddr, eoa.address)
+    const eoaTokenBalance = await getAddrBalanceErc(eoa, TOKEN_ADDRESS, eoa.address)
     const eoaATokenBalance = await getAddrBalanceErc(eoa, aTokenAddress, eoa.address)
     await mainTest(wallet, aTokenAddress)
 
-    const endEoaTokenBalance = await getAddrBalanceErc(eoa, tokenAddr, eoa.address)
+    const endEoaTokenBalance = await getAddrBalanceErc(eoa, TOKEN_ADDRESS, eoa.address)
     const endEoaATokenBalance = await getAddrBalanceErc(eoa, aTokenAddress, eoa.address)
     const walletATokenBalance = await getAddrBalanceErc(eoa, aTokenAddress, wallet.address)
     const walletAllowance = await getAllowanceErc(eoa, aTokenAddress, eoa.address, wallet.address)
@@ -261,20 +271,14 @@ const main = async (privKey, prefundAmt, APIKEY, rpcurl) => {
 }
 
 const processConsole = () => {
-    const [tokenAddr, privKey, prefundAmt, APIKEY, rpcurl] = process.argv.slice(2)
+    const [tokenAddr, PRIV_KEY, PREFUND_AMT, APIKEY, RPC_URL] = process.argv.slice(2)
 }
 
 
-const privKey = "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
-const prefundAmt = 0.3
-const APIKEY = "hnHevQR0y394nBprGrvNx4HgoZHUwMet5mXTOBhf"
-const rpcurl = "http://127.0.0.1:8545"
 
 
-const tokenAddr = DAI
 
-
-main(privKey, prefundAmt, APIKEY, rpcurl)
+main(PRIV_KEY, PREFUND_AMT, APIKEY, RPC_URL)
 // processConsole()
 
 
