606d259189e8b789132f65150bd38666c140340c
Chazzzzzzz <42802149+Chazzzzzzz@users.noreply.github.com>
2023-02-17 17:39:42 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { FunWallet } = require("./src/FunWallet")
 const { FunWalletConfig } = require("./src/FunWalletConfig")
 const Modules = require("./src/modules")
+
 module.exports = {
     FunWallet,
     FunWalletConfig,
