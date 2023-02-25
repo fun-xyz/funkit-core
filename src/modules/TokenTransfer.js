ad574a1fdb98d85edcfa5aa5de1a99d7f6e838a6
Your Name <calebsirak@gmail.com>
2023-02-24 18:01:40 -0800

    updated token

--- src/modules/TokenTransfer.js
+++ src/modules/TokenTransfer.js
@@ -5,7 +5,6 @@ const { PrimitiveModule } = require("./PrimitiveModule")
 
 class TokenTransfer extends PrimitiveModule {
     async createTransferTx(to, amount, ERC20Token) {
-        // console.log(this.wallet.config.chainId, this.wallet.config)
         const token = new Token({ address: ERC20Token, chainId: this.wallet.config.chainId.toString() })
         const ERC20Contract = new ethers.Contract(await token.getAddress(), ERC20.abi)
         const transferData = await ERC20Contract.populateTransaction.transfer(to, amount)
