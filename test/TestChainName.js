7b337259fc2e7a83d8b6b5b8d2b92c169be7ff25
James Rezendes <jamierezendes@gmail.com>
2023-03-10 14:15:44 -0800

    chain info on local asks local bundler

--- test/TestChainName.js
+++ test/TestChainName.js
@@ -1,30 +1,36 @@
 const { DataServer} = require('../utils/DataServer')
 const { API_KEY } = require("./TestUtils")
+const { expect } = require("chai")
 
 const dataServer = new DataServer(API_KEY);
 dataServer.init();
 
-async function main(){
-  console.log("Testing DataServer.getChainFromName");
-  try {
-    let chain = await dataServer.getChainFromName("ethereum");
-    console.log(chain)
-    chain = await dataServer.getChainFromName("avalanche");
-    console.log(chain)
-    chain = await dataServer.getChainFromName("avalanche-fuji");
-    console.log(chain)
-    chain = await dataServer.getChainFromName("polygon");
-    console.log(chain)
-    chain = await dataServer.getChainFromName("polygon-mumbai");
-    console.log(chain)
-    chain = await dataServer.getChainFromName("ethereum-localfork");
-    console.log(chain)
-    chain = await DataServer.getChainInfo("31337");
-    console.log(chain)
-  } catch(e){
-    console.log(e)
-  }
-}
+describe("Getting chain info", async function() {
+  it("succeed case", async function() {
+    try {
+      let chainFromName = await dataServer.getChainFromName("ethereum");
+      let chainFromId = await DataServer.getChainInfo(1);
+      expect(JSON.stringify(chainFromName)).to.be.equal(JSON.stringify(chainFromId))
+      chainFromName = await dataServer.getChainFromName("avalanche");
+      chainFromId = await DataServer.getChainInfo(43114);
+      expect(JSON.stringify(chainFromName)).to.be.equal(JSON.stringify(chainFromId))
+      chainFromName = await dataServer.getChainFromName("avalanche-fuji");
+      chainFromId = await DataServer.getChainInfo(43113);
+      expect(JSON.stringify(chainFromName)).to.be.equal(JSON.stringify(chainFromId))
+      chainFromName = await dataServer.getChainFromName("polygon");
+      chainFromId = await DataServer.getChainInfo(137);
+      expect(JSON.stringify(chainFromName)).to.be.equal(JSON.stringify(chainFromId))
+      chainFromName = await dataServer.getChainFromName("polygon-mumbai");
+      chainFromId = await DataServer.getChainInfo(80001);
+      expect(JSON.stringify(chainFromName)).to.be.equal(JSON.stringify(chainFromId))
+      chainFromName = await dataServer.getChainFromName("ethereum-localfork");
+      chainFromId = await DataServer.getChainInfo(31337);
+      expect(JSON.stringify(chainFromName)).to.be.equal(JSON.stringify(chainFromId))
+    } catch(e){
+      console.log(e)
+    }
+  })
 
-// main()
+})
+
 
