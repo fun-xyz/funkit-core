e3af082c0549e670dfa1a9aac571492857064ae1
aaronchewbani <aaronchewbani@gmail.com>
2023-03-16 14:21:06 -0700

    update

--- test/TestnetTest.config.js
+++ test/TestnetTest.config.js
@@ -7,14 +7,17 @@ const ETHEREUM={
         CHAIN:"5",
         PREFUNDAMT:0,
         ADAIADDRESS:"0xADD98B0342e4094Ec32f3b67Ccfd3242C876ff7a"
-    },
-    MAIN:{
-
     }
 }
 const AVALANCHE={
     FUJI:{
-
+        RPC:"https://avalanche-fuji.infura.io/v3/4a1a0a67f6874be6bb6947a62792dab7",
+        PRIVKEY:"6270ba97d41630c84de28dd8707b0d1c3a9cd465f7a2dba7d21b69e7a1981064", //ensure
+        TO:"0xDc054C4C5052F0F0c28AA8042BB333842160AEA2",
+        USDC_ADDR:"0x5425890298aed601595a70AB815c96711a31Bc65",
+        CHAIN:"43113",
+        PREFUNDAMT:0,
+        ADAIADDRESS:"0x7021eB315AD2Ce787E3A6FD1c4a136c9722457Cc"
     }
 }
 const POLYGON={
@@ -22,7 +25,7 @@ const POLYGON={
 
     },
     MAIN:{
-        
+
     }
 }
 
