db77a6f909bd99f863d0a10e9e504d3f0e6c9c8f
Your Name <calebsirak@gmail.com>
2023-03-30 15:59:26 -0700

    final pre demo launch build

---
+++ index.js
@@ -0,0 +1,6 @@
+const { configureEnvironment } = require("./managers");
+const { TokenSponsor } = require("./sponsors");
+const { FunWallet } = require("./wallet");
+
+
+module.exports = { configureEnvironment, TokenSponsor, FunWallet };
