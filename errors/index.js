93240bf39cef2e9d18cd26cc3ee7f81680123672
Chaz <42802149+Chazzzzzzz@users.noreply.github.com>
2023-03-30 15:06:38 -0700

    Update index.js

--- errors/index.js
+++ errors/index.js
@@ -5,5 +5,4 @@ const parameterErrors = require("./ParameterError")
 const serverErrors = require("./ServerError")
 const txErrors = require("./TransactionError")
 
-
 module.exports = { ...txErrors, ...serverErrors, ...dataErrors, ...baseErrors, ...helpers, ...parameterErrors }
