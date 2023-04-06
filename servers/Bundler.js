e5ee263e9a1f2bf41afd8dfef23095f4bd91ea06
Panda Xu <panda@fun.xyz>
2023-04-05 19:22:07 -0700

    Setup bundler RPC connections and APIs.
    implemented estimateUserOpGas
    
    migrate validateChainId
    
    update servers/DataServer.js
    
    Update DataServer.js

--- servers/Bundler.js
+++ servers/Bundler.js
@@ -2,6 +2,7 @@ const { resolveProperties } = require("ethers/lib/utils");
 const { JsonRpcProvider } = require("@ethersproject/providers");
 const { deepHexlify, verifyValidParametersForLocation } = require("../utils/data");
 const { Helper, NoServerConnectionError, ServerError, ParameterFormatError } = require("../errors");
+const { DataServer } = require('./DataServer');
 
 const bundlerExpectedKeys = ["bundlerUrl", "entryPointAddress", "chainId"]
 
@@ -14,40 +15,35 @@ class Bundler {
         this.chainId = chainId;
         this.userOpJsonRpcProvider = new JsonRpcProvider(this.bundlerUrl);
     }
-    async validateChainId() {
+    async validateChainId(chainId) {
         // validate chainId is in sync with expected chainid
-        let chain;
-        try {
-
-            chain = await this.userOpJsonRpcProvider.send('eth_chainId', []);
-
-        } catch (e) {
-            const helper = new Helper("Bundler Url", this.bundlerUrl, "Can not connect to bundler.")
-            throw new NoServerConnectionError("Chain.loadBundler", "Bundler", helper, this.key != "bundlerUrl")
-        }
-
-        if (parseInt(chain) != this.chainId && this.chainId != 1337) {
-            const helper = new Helper("Chain Id: ", this.chainId, `Bundler ${ this.bundlerUrl } is on chainId ${ chain }, but provider is on chainId ${ this.chainId }`)
-            throw new ParameterFormatError("Chain.loadBundler", helper)
-        }
+        return await DataServer.validateChainId(chainId);
     }
 
     async sendUserOpToBundler(userOp1) {
-        await this.validateChainId();
         const hexifiedUserOp = deepHexlify(await resolveProperties(userOp1));
-        return await this.userOpJsonRpcProvider
-            .send('eth_sendUserOperation', [hexifiedUserOp, this.entryPointAddress]);
+        const body = {
+            userOp: hexifiedUserOp,
+            entryPointAddress: this.entryPointAddress,
+            chainId: this.chainId
+        };
+        const response = await DataServer.sendUserOpToBundler(body);
+        return response;
     }
+
     async estimateUserOpGas(userOp1) {
-        await this.validateChainId();
         const hexifiedUserOp = deepHexlify(await resolveProperties(userOp1));
-        return await this.userOpJsonRpcProvider.send('eth_estimateUserOperationGas', [hexifiedUserOp, this.entryPointAddress]);
+        const body = {
+            userOp: hexifiedUserOp,
+            entryPointAddress: this.entryPointAddress,
+            chainId: this.chainId
+        };
+        const response = await DataServer.sendUserOpToBundler(body);
+        return response;
     }
 
     static async getChainId(bundlerUrl) {
-        const provider = new JsonRpcProvider(bundlerUrl);
-        const chain = await provider.send('eth_chainId', []);
-        return parseInt(chain);
+        return await DataServer.getChainId(bundlerUrl);
     }
 }
 
