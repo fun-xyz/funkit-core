fe077635588969cb8c7b7c3000029287c4bddfdc
Aaron Chew <aaronchewbani@gmail.com>
2023-04-20 17:18:00 -0700

    Delete Factory.js

--- test/bsc/Factory.js
+++
@@ -1,7 +0,0 @@
-const { FactoryTest } = require('../testUtils/Factory.js')
-const { WALLET_PRIVATE_KEY, TEST_PRIVATE_KEY } = require("../../utils")
-const config = {
-    chainId: 56,
-    authPrivateKey: TEST_PRIVATE_KEY,
-}
-FactoryTest(config)
