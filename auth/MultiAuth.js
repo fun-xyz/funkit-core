2bdfb140c86abd3deec603f04943b0508ea41456
chaz <chaz@chazs-MacBook-Pro.local>
2023-04-21 14:45:02 -0700

    add multiAuth

---
+++ auth/MultiAuth.js
@@ -0,0 +1,33 @@
+const { v4: uuidv4 } = require('uuid');
+const { Eoa } = require("./EoaAuth");
+const { arrayify } = require("ethers/lib/utils");
+
+class MultiAuth extends Eoa {
+    constructor(input) {
+        super(input)
+        this.ids = input.ids //["twitter###Chazzz", "google###chaz@fun.xyz", "0x111111111111111111"]
+        this.provider = input.provider
+        this.uniqueID = uuidv4()
+    }
+
+    // figure storage part
+
+    async getUniqueID() {
+        return this.uniqueID
+    }
+
+    async getSigner() {
+        this.signer = await this.provider.getSigner()
+        return this.signer
+    }
+
+    async getOwnerAddr() {
+        const signer = await this.getSigner()
+        return await signer.getAddress()
+    }
+
+    async signHash(hash) {
+        const signer = await this.getSigner()
+        return await signer.signMessage(arrayify(hash))
+    }
+}
