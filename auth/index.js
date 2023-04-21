1581984778d22dbceac49e55cee5241f6fa0fc5c
James Rezendes <jamierezendes@gmail.com>
2023-04-21 11:53:17 -1000

    export multiauth

--- auth/index.js
+++ auth/index.js
@@ -1,5 +1,6 @@
 const Auth = require("./Auth")
 const EoaAuth = require("./EoaAuth")
 const MagicAuthEoa = require("./MagicAuthEoa")
+const MultiAuthEOA = require("./MultiAuthEoa");
 
-module.exports = { ...Auth, ...EoaAuth, ...MagicAuthEoa };
+module.exports = { ...Auth, ...EoaAuth, ...MagicAuthEoa, ...MultiAuthEOA };
