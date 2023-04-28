857a569742e49400911e1b81f75fe4a81c34beff
Chaz <42802149+Chazzzzzzz@users.noreply.github.com>
2023-04-28 14:24:03 -0700

    Update DataServer.js

--- servers/DataServer.js
+++ servers/DataServer.js
@@ -47,7 +47,7 @@ class DataServer {
             receipt,
             organization: global.orgInfo?.id,
             orgName: global.orgInfo?.name,
-            chainId:global.chain?.id
+            chainId: global.chain?.id
         }
         await this.sendPostRequest(APIURL, "save-user-op", body).then((r) => {
         })
