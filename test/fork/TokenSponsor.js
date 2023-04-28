0a2863c9db6106db045e331bda2a9add17ed742a
Chaz <42802149+Chazzzzzzz@users.noreply.github.com>
2023-04-28 14:24:30 -0700

    Update TokenSponsor.js

--- test/fork/TokenSponsor.js
+++ test/fork/TokenSponsor.js
@@ -126,7 +126,3 @@ describe("TokenSponsor", function () {
 
 
 })
-
-// 0xCb7C8BF1a08b1B631051f80c2f7C4566F3ad785D
-// 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266
-// 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48
