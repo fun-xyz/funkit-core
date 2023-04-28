cafb2ad1332a3c576b163363dbbbb352557c62b6
chaz <chaz@chazs-MacBook-Pro.local>
2023-04-27 17:19:05 -0700

    remove hardcoded factory address

--- test/forkDefaults.js
+++ test/forkDefaults.js
@@ -1,7 +1,6 @@
 const defaultAddresses = {
     "1inchOracleAddress": "0x07D91f5fb9Bf7798734C3f606dB065549F6893bb",
-    "feeOracle": "0x6A2F81c62DC64C6E2b9DC411Af64ADEA926e472C",
-    "factoryAddress": "0xc96Ee0f558D12C3a4B082E9B759Ab8128e2e5206",
+    "feeOracle": "0x6A2F81c62DC64C6E2b9DC411Af64ADEA926e472C"
 }
 
 module.exports = { defaultAddresses };
