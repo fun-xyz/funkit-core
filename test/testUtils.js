c41a1093d29fce2d613a658e7f7681940eecab4f
Albert Su <albertsu@Alberts-MacBook-Pro-2.local>
2023-05-15 00:29:57 -0600

    Format PR

--- test/testUtils.js
+++ test/testUtils.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+// require('dotenv').config();
 
 const FUN_TESTNET_CHAIN_ID = 36864
 const LOCAL_FORK_CHAIN_ID = 31337
@@ -21,7 +21,6 @@ async function getTestApiKey() {
 }
 // use env for pkeys
 module.exports = {
-  FUN_TESTNET_CHAIN_ID,
-  LOCAL_FORK_CHAIN_ID, FUN_TESTNET_CHAIN_KEY, LOCAL_FORK_CHAIN_KEY, FUN_TESTNET_RPC_URL, LOCAL_FORK_RPC_URL,
+  FUN_TESTNET_CHAIN_ID, LOCAL_FORK_CHAIN_ID, FUN_TESTNET_CHAIN_KEY, LOCAL_FORK_CHAIN_KEY, FUN_TESTNET_RPC_URL, LOCAL_FORK_RPC_URL,
   getTestApiKey, ...process.env
 };
