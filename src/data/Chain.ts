390fc0f84271d7b5363dea1b4fc3cd6c8e6a68e2
30PandaX <panda@fun.xyz>
2023-06-02 16:55:51 -0700

    Update Chain.ts

--- src/data/Chain.ts
+++ src/data/Chain.ts
@@ -90,7 +90,7 @@ export class Chain {
                 const abisAddresses = this.loadAddressesFromAbis(chainId)
                 const addresses = { ...chain.aaData, ...flattenObj(chain.moduleAddresses), ...abisAddresses }
                 Object.assign(this, { ...this, addresses, ...chain.rpcdata })
-                // this.modifyAddresses()
+                this.modifyAddresses()
             }
         } catch (e) {
             const helper = new Helper("getChainInfo", chain, "call failed")
