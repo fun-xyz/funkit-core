db74e4869ecd312025a06ce2eb22e302d294156b
Albert Su <albertsu2001@gmail.com>
2023-06-04 18:42:38 -0700

    Fix stake tests

--- tests/goerli/Stake.ts
+++ tests/goerli/Stake.ts
@@ -5,7 +5,6 @@ dotenv.config()
 const PREFUND = process.env.PREFUND === "true" ? true : false
 const config: StakeTestConfig = {
     chainId: 5,
-    authPrivateKey: process.env.WALLET_PRIVATE_KEY!,
     baseToken: "eth",
     prefund: PREFUND,
     steth: "0x1643E812aE58766192Cf7D2Cf9567dF2C37e9B7F"
