97e63059abcfa73497dfbbe9d862a52542ab771b
aaronchewbani <aaronchewbani@gmail.com>
2023-06-04 23:43:46 -0700

    apiserver

--- tests/IntegrationTests/apis/InfoApis.ts
+++ tests/IntegrationTests/apis/InfoApis.ts
@@ -1,7 +1,7 @@
-import { configureEnvironment, GlobalEnvOption} from '../../../src/config'
-import {getTokenInfo, getChainInfo, getChainFromName, getModuleInfo, getPaymasterAddress} from '../../../src/apis/InfoApis'
-const { expect } = require('chai')
-import { getTestApiKey } from "../../getTestApiKey"
+import { expect } from "chai"
+import { getChainFromName, getChainInfo, getModuleInfo, getPaymasterAddress, getTokenInfo } from "../../../src/apis/InfoApis"
+import { GlobalEnvOption, configureEnvironment } from "../../../src/config"
+import { getTestApiKey } from "../../getAWSSecrets"
 
 describe("InfoApis Test", function () {
     this.timeout(100_000)
@@ -9,35 +9,35 @@ describe("InfoApis Test", function () {
     before(async () => {
         const apiKey = await getTestApiKey()
         const options: GlobalEnvOption = {
-            chain: '5',
+            chain: "5",
             apiKey: apiKey
         }
         await configureEnvironment(options)
     })
     it("getTokenInfo", async () => {
-        const tokenInfo = await getTokenInfo('usdc', '5')
-        expect(tokenInfo).to.be.equal('0x07865c6E87B9F70255377e024ace6630C1Eaa37F')
+        const tokenInfo = await getTokenInfo("usdc", "5")
+        expect(tokenInfo).to.be.equal("0x07865c6E87B9F70255377e024ace6630C1Eaa37F")
     })
 
     it("getChainInfo", async () => {
-        const chainInfo = await getChainInfo('5')
-        expect(chainInfo).to.have.property('chain', '5')
-        expect(chainInfo).to.have.property('aaData')
+        const chainInfo = await getChainInfo("5")
+        expect(chainInfo).to.have.property("chain", "5")
+        expect(chainInfo).to.have.property("aaData")
     })
 
     it("getChainFromName", async () => {
-        const chainInfo = await getChainFromName('ethereum-goerli')
-        expect(chainInfo).to.have.property('chain', '5')
-        expect(chainInfo).to.have.property('aaData')
+        const chainInfo = await getChainFromName("ethereum-goerli")
+        expect(chainInfo).to.have.property("chain", "5")
+        expect(chainInfo).to.have.property("aaData")
     })
-    
+
     it("getModuleInfo", async () => {
-        const moduleInfo = await getModuleInfo('paymaster','5')
-        expect(moduleInfo).to.have.property('oracle')
+        const moduleInfo = await getModuleInfo("paymaster", "5")
+        expect(moduleInfo).to.have.property("oracle")
     })
 
     it("getPaymasterAddress", async () => {
-        const paymasterAddress = await getPaymasterAddress('5')
-        expect(paymasterAddress).to.have.property('oracle')
+        const paymasterAddress = await getPaymasterAddress("5")
+        expect(paymasterAddress).to.have.property("oracle")
     })
 })
