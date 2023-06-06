2fd35e9cc4c23c00cac6c34e97d4de9a3fc3e814
30PandaX <panda@fun.xyz>
2023-06-06 11:30:59 -0700

    Update BaseError.ts

--- src/errors/BaseError.ts
+++ src/errors/BaseError.ts
@@ -12,12 +12,12 @@ export class BaseError extends Error {
     }
 
     loadEnd() {
-        const version = "v0.2.7";
+        const version = "v0.2.7"
         const email = "support@fun.xyz"
         const twttr = "https://twitter.com/fun"
         const phone = "(510) 585-3423"
-        const infoLine = `Visit https://docs.fun.xyz for documentation about this command.\nEmail: ${email}\nTwitter: ${twttr}\nPhone Number: ${phone}`;
+        const infoLine = `Visit https://docs.fun.xyz for documentation about this command.\nEmail: ${email}\nTwitter: ${twttr}\nPhone Number: ${phone}`
         this.message +=
-            `\nThis is an internal sdk error. Please contact the fun team at these contacts for the fastest response.\nversion: ${version}\ninfo: ${infoLine}`;
+            `\nThis is an internal sdk error. Please contact the fun team at these contacts for the fastest response.\nversion: ${version}\ninfo: ${infoLine}`
     }
 }
