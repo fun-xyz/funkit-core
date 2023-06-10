00aabf413c4b6ea2b07d0e003964170d140d3438
Your Name <calebsirak@gmail.com>
2023-06-09 17:02:17 -0700

    added eoa with client passed in

--- src/auth/MagicAuthEoa.ts
+++ src/auth/MagicAuthEoa.ts
@@ -1,6 +1,7 @@
-import { Address, Hex } from "viem"
+import { Address, Hex, pad, toBytes } from "viem"
 import { Eoa } from "./EoaAuth"
 import { EoaAuthInput } from "./types"
+import { WalletSignature, encodeWalletSignature } from "../data"
 
 export interface MagicAuthEoaInput extends EoaAuthInput {
     uniqueId: Hex
@@ -25,5 +26,31 @@ export class MagicAuthEoa extends Eoa {
 
     override async getAddress(): Promise<Address> {
         return await this.getOwnerAddr()[0]
+    }
+
+    override async getEstimateGasSignature(): Promise<Hex> {
+        await this.init()
+        const walletSignature: WalletSignature = {
+            userId: await this.getAddress(),
+            signature: pad("0x", { size: 65 })
+        }
+        return encodeWalletSignature(walletSignature)
+    }
+
+    override async signHash(hash: Hex): Promise<Hex> {
+        await this.init()
+        let signature
+        if (this.signer?.type === "local") {
+            signature = await this.signer.signMessage({ message: { raw: toBytes(hash) } })
+        } else if (this.client && this.account) {
+            signature = await this.client.signMessage({ account: this.account, message: hash })
+        } else {
+            throw new Error("No signer or client")
+        }
+        const walletSignature: WalletSignature = {
+            userId: await this.getAddress(),
+            signature: signature
+        }
+        return encodeWalletSignature(walletSignature)
     }
 }
