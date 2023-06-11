2b0f94009c7f42b955b3d81fcfbc5b1333bee904
Albert Su <albertsu2001@gmail.com>
2023-06-10 22:22:05 -0700

    All tests passing except nft and gasless sponsor

--- tests/optimismGoerli/GaslessSponsor.ts
+++ tests/optimismGoerli/GaslessSponsor.ts
@@ -4,7 +4,7 @@ const config: GaslessSponsorTestConfig = {
     chainId: 420,
     inToken: "eth",
     outToken: "usdc",
-    stakeAmount: 0.0005,
+    stakeAmount: 0.05,
     prefund: false
 }
 GaslessSponsorTest(config)
