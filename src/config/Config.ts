84a43590e928eeacbf10128a7e0b480f2e56c06e
chaz <chaz@fun.xyz>
2023-06-12 14:01:46 -0700

    fix global config

--- src/config/Config.ts
+++ src/config/Config.ts
@@ -33,7 +33,7 @@ export function parseOptions(option: EnvOption) {
 
 export async function configureEnvironment(option: GlobalEnvOption) {
     const global = globalThis as any
-    global.globalEnvOption = global.globalEnvOption ? global.globalEnvOption : {}
+    global.globalEnvOption = {}
     const globalEnvOption = global.globalEnvOption as GlobalEnvOption
     if (!option.chain && !globalEnvOption.chain) {
         globalEnvOption.chain = await getChainFromData(5)
@@ -48,7 +48,9 @@ export async function configureEnvironment(option: GlobalEnvOption) {
     }
 
     globalEnvOption.orgInfo = await getOrgInfo(globalEnvOption.apiKey!)
-    globalEnvOption.gasSponsor = option.gasSponsor ? option.gasSponsor : globalEnvOption.gasSponsor
+    globalEnvOption.gasSponsor =
+        option.gasSponsor === null || option.gasSponsor === undefined ? globalEnvOption.gasSponsor : option.gasSponsor
     globalEnvOption.fee = option.fee ? option.fee : globalEnvOption.fee
-    globalEnvOption.sendTxLater = option.sendTxLater ? option.sendTxLater : globalEnvOption.sendTxLater
+    globalEnvOption.sendTxLater =
+        option.sendTxLater === null || option.sendTxLater === undefined ? globalEnvOption.sendTxLater : option.sendTxLater
 }
