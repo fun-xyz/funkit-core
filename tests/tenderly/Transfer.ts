1c948b7bbd477b7d2ed69cadaf5bba7bc927cf55
Albert Su <albertsu2001@gmail.com>
2023-06-13 01:20:06 -0700

    🧹Remove Tenderly

--- tests/tenderly/Transfer.ts
+++
@@ -1,9 +0,0 @@
-import { TransferTest, TransferTestConfig } from "../testUtils/Transfer"
-
-const config: TransferTestConfig = {
-    chainId: 36865,
-    outToken: "dai",
-    baseToken: "eth",
-    prefund: false
-}
-TransferTest(config)
