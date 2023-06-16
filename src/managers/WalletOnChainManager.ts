93753916aea23acaa663d6fbc80e6fb552e452d5
aaronchewbani <aaronchewbani@gmail.com>
2023-06-15 18:43:42 -0700

    add txid support

--- src/managers/WalletOnChainManager.ts
+++ src/managers/WalletOnChainManager.ts
@@ -35,9 +35,7 @@ export class WalletOnChainManager {
         let filter
         while (Date.now() < endtime) {
             let events
-            if ((await client.getChainId()) === 36865) {
-                return "0x"
-            } else if ((await client.getChainId()) === 84531) {
+            if ((await client.getChainId()) === 84531 || (await client.getChainId()) === 36865) {
                 events = await ENTRYPOINT_CONTRACT_INTERFACE.getLog(
                     entryPointAddress,
                     "UserOperationEvent",
