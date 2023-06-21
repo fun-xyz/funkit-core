d5e74e1ba116294112548d260be9970d2d4d499a
aaronchewbani <aaronchewbani@gmail.com>
2023-06-21 14:30:47 -0700

    push

--- src/apis/PaymasterApis.ts
+++ src/apis/PaymasterApis.ts
@@ -1,11 +1,10 @@
-import { API_URL } from "../common/constants"
+import { DASHBOARD_API_URL } from "../common/constants"
 import { PaymasterType } from "../sponsors/types"
 import { sendPostRequest } from "../utils/ApiUtils"
 export interface PaymasterTransaction {
     action: string
     amount: number
     from: string
-    timestamp: number
     to: string
     token: string
     txid?: string
@@ -17,7 +16,7 @@ export async function updatePaymasterMode(
     paymasterType: PaymasterType,
     sponsorAddress: string
 ): Promise<any> {
-    return await sendPostRequest(API_URL, "paymasters/update-paymaster", {
+    return await sendPostRequest(DASHBOARD_API_URL, "paymasters/update", {
         chain: chainId,
         sponsorAddress,
         type: paymasterType,
@@ -32,7 +31,7 @@ export async function removeFromList(
     paymasterType: PaymasterType,
     sponsorAddress: string
 ): Promise<any> {
-    return await sendPostRequest(API_URL, "paymasters/remove-from-list", {
+    return await sendPostRequest(DASHBOARD_API_URL, "paymasters/remove-from-list", {
         chain: chainId,
         sponsorAddress,
         type: paymasterType,
@@ -48,7 +47,7 @@ export async function addToList(
     paymasterType: PaymasterType,
     sponsorAddress: string
 ): Promise<any> {
-    return await sendPostRequest(API_URL, "paymasters/add-to-list", {
+    return await sendPostRequest(DASHBOARD_API_URL, "paymasters/add-to-list", {
         chain: chainId,
         sponsorAddress,
         type: paymasterType,
@@ -59,20 +58,24 @@ export async function addToList(
 
 export async function addTransaction(
     chainId: string,
+    timestamp: number,
+    txid,
     transaction: PaymasterTransaction,
     paymasterType: PaymasterType,
     sponsorAddress: string
 ): Promise<any> {
-    return await sendPostRequest(API_URL, "paymasters/add-transaction", {
+    return await sendPostRequest(DASHBOARD_API_URL, "paymasters/add-transaction", {
         chain: chainId,
         sponsorAddress,
         type: paymasterType,
-        transaction
+        timestamp,
+        transaction,
+        txid
     })
 }
 
 export async function addPaymasterToken(chainId: string, tokenAddress: string): Promise<any> {
-    return await sendPostRequest(API_URL, "paymasters/add-supported-token", {
+    return await sendPostRequest(DASHBOARD_API_URL, "paymasters/supported-tokens/add", {
         chain: chainId,
         tokenAddress
     })
--- tests/testScript.ts
+++ tests/testScript.ts
@@ -8,15 +8,15 @@ const PRIVATE_KEY = "0x6270ba97d41630c84de28dd8707b0d1c3a9cd465f7a2dba7d21b69e7a
 async function main() {
     await configureEnvironment({
         chain: "5",
-        apiKey: API_KEY,
-        gasSponsor: {
-            sponsorAddress: "0x1c1adFD4b2c21D8a1e39153D394d8309Db383E43",
-            token: "0x855Af47Cdf980A650aDE1ad47c78ec1DEEbe9093"
-        }
+        apiKey: API_KEY
+        // gasSponsor: {
+        //     sponsorAddress: "0x1c1adFD4b2c21D8a1e39153D394d8309Db383E43",
+        //     token: "0x855Af47Cdf980A650aDE1ad47c78ec1DEEbe9093"
+        // }
     })
     const auth = new Eoa({ privateKey: PRIVATE_KEY })
     const uniqueId = await auth.getUniqueId()
-    const funWallet = new FunWallet({ uniqueId, index: 13230 })
+    const funWallet = new FunWallet({ uniqueId, index: 13323230 })
     console.log(await funWallet.getAddress())
 
     const sponsor = new TokenSponsor({
@@ -31,18 +31,14 @@ async function main() {
     // await funWallet.execute(auth, approve)
 
     // // 2. Stake the token and the amount
-    // const stake = await sponsor.stakeToken(
-    //     "0x855Af47Cdf980A650aDE1ad47c78ec1DEEbe9093",
-    //     "0xeb32d09f0B46548Cdc604309D0611F05D8f706D3", //Sponsor address
-    //     200
-    // )
-    // await funWallet.execute(auth, stake)
+    const stake = await sponsor.stake("0x855Af47Cdf980A650aDE1ad47c78ec1DEEbe9093", 0.03)
+    await funWallet.execute(auth, stake)
 
-    console.log(await sponsor.getTokenBalance("0x855Af47Cdf980A650aDE1ad47c78ec1DEEbe9093", "0xeb32d09f0B46548Cdc604309D0611F05D8f706D3"))
+    // console.log(await sponsor.getTokenBalance("0x855Af47Cdf980A650aDE1ad47c78ec1DEEbe9093", "0xeb32d09f0B46548Cdc604309D0611F05D8f706D3"))
 
     // const call = sponsor.setToBlacklistMode()
     // await funWallet.execute(auth, call)
-    console.log(await funWallet.transfer(auth, { to: "0x175C5611402815Eba550Dad16abd2ac366a63329", amount: 0.01, token: "eth" }))
+    // console.log(await funWallet.transfer(auth, { to: "0x175C5611402815Eba550Dad16abd2ac366a63329", amount: 0.01, token: "eth" }))
 }
 
 main()
