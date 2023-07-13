477dd0fbbcd8fe1e160153f83897cc38af60aff9
Your Name <calebsirak@gmail.com>
2023-07-13 15:14:31 -0700

    added session key

--- src/actions/Token.ts
+++ src/actions/Token.ts
@@ -4,14 +4,14 @@ import { ERC20_CONTRACT_INTERFACE, ERC721_CONTRACT_INTERFACE, WALLET_CONTRACT_IN
 
 export const erc721TransferCalldata = async (params: ERC721TransferParams, walletAddress: Address): Promise<Hex> => {
     const { to, tokenId, token } = params
-    const transferData = await ERC721_CONTRACT_INTERFACE.encodeTransactionData(token, "transferFrom", [walletAddress, to, tokenId])
+    const transferData = ERC721_CONTRACT_INTERFACE.encodeTransactionData(token, "transferFrom", [walletAddress, to, tokenId])
 
     return WALLET_CONTRACT_INTERFACE.encodeData("execFromEntryPoint", [token, 0, transferData.data])
 }
 
 export const erc20TransferCalldata = async (params: ERC20TransferParams): Promise<Hex> => {
     const { to, amount, token } = params
-    const transferData = await ERC20_CONTRACT_INTERFACE.encodeTransactionData(token, "transfer", [to, amount])
+    const transferData = ERC20_CONTRACT_INTERFACE.encodeTransactionData(token, "transfer", [to, amount])
     return WALLET_CONTRACT_INTERFACE.encodeData("execFromEntryPoint", [token, 0, transferData.data])
 }
 
@@ -21,12 +21,12 @@ export const ethTransferCalldata = async (params: NativeTransferParams): Promise
 
 export const erc20ApproveCalldata = async (params: ApproveERC20Params): Promise<Hex> => {
     const { spender, amount, token } = params
-    const approveData = await ERC20_CONTRACT_INTERFACE.encodeTransactionData(token, "approve", [spender, amount])
+    const approveData = ERC20_CONTRACT_INTERFACE.encodeTransactionData(token, "approve", [spender, amount])
     return WALLET_CONTRACT_INTERFACE.encodeData("execFromEntryPoint", [token, 0, approveData.data])
 }
 
 export const erc721ApproveCalldata = async (params: ApproveERC721Params): Promise<Hex> => {
     const { spender, tokenId, token } = params
-    const approveData = await ERC721_CONTRACT_INTERFACE.encodeTransactionData(token, "approve", [spender, tokenId])
+    const approveData = ERC721_CONTRACT_INTERFACE.encodeTransactionData(token, "approve", [spender, tokenId])
     return WALLET_CONTRACT_INTERFACE.encodeData("execFromEntryPoint", [token, 0, approveData.data])
 }
