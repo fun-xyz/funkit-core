01d993a5e73ecb9bd9ea8668e809788238da58f3
aaronchewbani <aaronchewbani@gmail.com>
2023-07-24 17:01:30 -0700

    moonpay sdk change

--- src/apis/OnOffRampUtils.ts
+++ src/apis/OnOffRampUtils.ts
@@ -2,9 +2,9 @@ import { API_URL } from "../common/constants"
 import { sendGetRequest } from "../utils/ApiUtils"
 
 export async function getOnRampUrl(walletAddr: string): Promise<void> {
-    return (await sendGetRequest(API_URL, `moonpay/buyUrl/${walletAddr}`))?.url
+    return (await sendGetRequest(API_URL, `on-ramp/${walletAddr}?provider=moonpay`))?.url
 }
 
 export async function getOffRampUrl(walletAddr: string): Promise<void> {
-    return (await sendGetRequest(API_URL, `moonpay/sellUrl/${walletAddr}`))?.url
+    return (await sendGetRequest(API_URL, `off-ramp/${walletAddr}?provider=moonpay`))?.url
 }
