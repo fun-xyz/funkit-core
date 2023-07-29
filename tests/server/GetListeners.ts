2a74182745e4aa2ea903ab8fd66e28dd2b9749a2
aaronchewbani <aaronchewbani@gmail.com>
2023-07-29 14:50:02 -0700

    fix unit tests

--- tests/server/GetListeners.ts
+++ tests/server/GetListeners.ts
@@ -26,17 +26,13 @@ describe("Get Operations", function () {
                     ["5"],
                     "https://webhook.site/#!/0d77df73-13e3-44d0-b2cc-65d0954d679d"
                 )
-                expect(res).to.deep.equal({
-                    success: "Successfully created listener"
-                })
+                expect(res).to.deep.equal("Successfully created listener")
             })
         })
         describe("Positive Unit Tests", () => {
             it("deleteListener", async () => {
                 const res = await deleteListener("0x62033e31048e2A82569a3e90E258e962E0510DfB", "5")
-                expect(res).to.deep.equal({
-                    success: "Successfully deleted listener"
-                })
+                expect(res).to.deep.equal("Successfully deleted listener")
             })
         })
     })
