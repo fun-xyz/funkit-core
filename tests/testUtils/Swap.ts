fea53783ac5ecb49e557b1d534a8bc785ed5fe06
aaronchewbani <aaronchewbani@gmail.com>
2023-08-11 15:51:08 -0700

    push change

--- tests/testUtils/Swap.ts
+++ tests/testUtils/Swap.ts
@@ -78,8 +78,7 @@ export const SwapTest = (config: SwapTestConfig) => {
                 returnAddress: walletAddress,
                 chainId: config.chainId
             })
-            const rec = await wallet.executeOperation(auth, operation)
-            console.log(rec)
+            await wallet.executeOperation(auth, operation)
             const tokenBalanceAfter = await Token.getBalanceBN(inToken, walletAddress)
             assert(tokenBalanceAfter > tokenBalanceBefore, "Swap did not execute")
         })
