ba28fd95e3a6149fcb19dbadd5acbe6a096ba385
Albert Su <albertsu2001@gmail.com>
2023-08-14 21:23:00 -0700

    ✨Working test

--- src/actions/LimitOrder.ts
+++ src/actions/LimitOrder.ts
@@ -1,6 +1,11 @@
 import { encodeAbiParameters } from "viem"
 import { LimitOrderParam } from "./types"
-import { TransactionParams, UNISWAP_V3_LIMIT_ORDER_CONTRACT_INTERFACE } from "../common"
+import {
+    APPROVE_AND_EXEC_CONTRACT_INTERFACE,
+    ERC20_CONTRACT_INTERFACE,
+    TransactionParams,
+    UNISWAP_V3_LIMIT_ORDER_CONTRACT_INTERFACE
+} from "../common"
 import { Chain, Token } from "../data"
 
 export const LimitOrderTransactionParams = async (
@@ -11,6 +16,7 @@ export const LimitOrderTransactionParams = async (
     const tokenInAddress = await new Token(tokenIn).getAddress()
     const tokenOutAddress = await new Token(tokenOut).getAddress()
     const chain = await Chain.getChain({ chainIdentifier: chainId })
+    const uniswapv3LimitOrderAddress = await chain.getAddress("uniswapv3LimitOrder")
     const approveAndExecAddress = await chain.getAddress("approveAndExecAddress")
     const data = encodeAbiParameters(
         [
@@ -21,5 +27,22 @@ export const LimitOrderTransactionParams = async (
         ],
         [[poolFee ?? 3000, tokenInAddress, tokenOutAddress, tokenInAmount, tokenOutAmount]]
     )
-    return UNISWAP_V3_LIMIT_ORDER_CONTRACT_INTERFACE.encodeTransactionParams(approveAndExecAddress, "execute", [data])
+
+    const UniswapV3LimitOrderExecuteData = UNISWAP_V3_LIMIT_ORDER_CONTRACT_INTERFACE.encodeTransactionParams(
+        uniswapv3LimitOrderAddress,
+        "execute",
+        [data]
+    ).data
+    const approveData = ERC20_CONTRACT_INTERFACE.encodeTransactionParams(tokenInAddress, "approve", [
+        uniswapv3LimitOrderAddress,
+        tokenInAmount
+    ]).data
+    const approveAndExecData = APPROVE_AND_EXEC_CONTRACT_INTERFACE.encodeTransactionParams(approveAndExecAddress, "approveAndExecute", [
+        uniswapv3LimitOrderAddress,
+        0,
+        UniswapV3LimitOrderExecuteData,
+        tokenInAddress,
+        approveData
+    ])
+    return approveAndExecData
 }
