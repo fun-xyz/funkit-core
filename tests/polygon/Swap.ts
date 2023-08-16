d3bbdcead78e8dcc37f80b8ccf6781a242328737
aaronchewbani <aaronchewbani@gmail.com>
2023-08-16 14:16:18 -0700

    job is done.

--- tests/polygon/Swap.ts
+++ tests/polygon/Swap.ts
@@ -7,6 +7,7 @@ const config: SwapTestConfig = {
     baseToken: "matic",
     amount: 0.0001,
     erc20toerc20Amt: 0.00001,
-    erc20toethAmt: 0.0001
+    erc20toethAmt: 0.0001,
+    prefundAmt: 1
 }
 SwapTest(config)
