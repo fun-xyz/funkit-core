9364e4e0fe1be195359917e8d9dee4434d4331c6
Albert Su <albertsu2001@gmail.com>
2023-08-28 21:37:35 -0700

    ✅Fix limitorder on goerli - failing bc uniswap pool ran out of tokens

--- tests/goerli/LimitOrder.ts
+++ tests/goerli/LimitOrder.ts
@@ -3,9 +3,9 @@ import { LimitOrderConfig, LimitOrderTest } from "../testUtils/LimitOrder"
 const config: LimitOrderConfig = {
     chainId: 5,
     outToken: "dai",
-    baseToken: "usdc",
-    tokenInAmount: 100,
-    tokenOutAmount: 1,
+    baseToken: "eth",
+    tokenInAmount: 0.001,
+    tokenOutAmount: 0.00001,
     prefundAmt: 0.2
 }
 LimitOrderTest(config)
