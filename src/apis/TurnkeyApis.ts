1baf141e5bc0b5c9a65a4ee232aceea52b7a7e43
Albert Su <albertsu2001@gmail.com>
2023-10-31 12:54:10 -0700

    ✨Use POST request for turnkey

--- src/apis/TurnkeyApis.ts
+++ src/apis/TurnkeyApis.ts
@@ -1,19 +1,13 @@
 import { API_URL } from "../common/constants"
-import { sendGetRequest } from "../utils/ApiUtils"
+import { sendPostRequest } from "../utils/ApiUtils"
 
 export async function createSubOrg(createSubOrgRequest: any): Promise<string> {
-    const params = new URLSearchParams({
-        createSubOrgRequest
-    }).toString()
-    const res = await sendGetRequest(API_URL, `turnkey/createSubOrg/?${params}`)
+    const res = await sendPostRequest(API_URL, "turnkey/createSubOrg/", createSubOrgRequest)
     return res.newSubOrgId
 }
 
 export async function createPrivateKey(signedRequest: any): Promise<any> {
-    const params = new URLSearchParams({
-        signedRequest
-    }).toString()
-    const res = await sendGetRequest(API_URL, `turnkey/createPrivateKey/?${params}`)
+    const res = await sendPostRequest(API_URL, "turnkey/createPrivateKey/", signedRequest)
     return {
         id: res.id,
         address: res.address
