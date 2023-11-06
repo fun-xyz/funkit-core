1a00a38750886b9d8b2c4f83d131c547421292ab
Albert Su <albertsu2001@gmail.com>
2023-11-06 16:28:00 -0500

    ✅Change any to object

--- src/apis/TurnkeyApis.ts
+++ src/apis/TurnkeyApis.ts
@@ -1,12 +1,12 @@
 import { API_URL } from "../common/constants"
 import { sendPostRequest } from "../utils/ApiUtils"
 
-export async function createTurnkeySubOrg(createSubOrgRequest: any): Promise<string> {
+export async function createTurnkeySubOrg(createSubOrgRequest: object): Promise<string> {
     const res = await sendPostRequest(API_URL, "turnkey/createSubOrg", createSubOrgRequest)
     return res
 }
 
-export async function createTurnkeyPrivateKey(signedRequest: any): Promise<any> {
+export async function createTurnkeyPrivateKey(signedRequest: object): Promise<any> {
     const res = await sendPostRequest(API_URL, "turnkey/createPrivateKey", signedRequest)
     return {
         id: res.id,
